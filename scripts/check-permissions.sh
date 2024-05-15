f4f067e8bf9858345c5d6922d40dddca4acbafd4
Dave Bloom <dave@digitalnoise.net>
2024-05-15 16:26:56 -0700

    chore: Fix token permissions error and improve error message in check-permissions.sh script

--- scripts/check-permissions.sh
+++ scripts/check-permissions.sh
@@ -4,4 +4,4 @@ if [ -f .env ]; then
     source .env
 fi
 
-curl -sS -f -I -H "Authorization: token ${COPILOT_ACCESS_LIST}" https://api.github.com | awk -F': ' '/x-oauth-scopes/{print $2}' | tr -d ' ' | tr -d '\n'
+curl -sS -f -I -H "Authorization: token ${COPILOT_ACCESS_LIST}" https://api.github.com | awk -F': ' '/x-oauth-scopes/{print $2}' | tr -d '\n' | tr -d ' '
