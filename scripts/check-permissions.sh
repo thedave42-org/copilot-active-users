2c0797d11fcf91e3258fe15c0f2ace4179d879b6
Dave Bloom <dave@digitalnoise.net>
2024-05-15 15:05:28 -0700

    chore: Update check-active-users workflow to use COPILOT_ACCESS_LIST for API authorization

--- scripts/check-permissions.sh
+++ scripts/check-permissions.sh
@@ -4,4 +4,4 @@ if [ -f .env ]; then
     source .env
 fi
 
-curl -sS -f -I -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com | awk -F': ' '/x-oauth-scopes/{print $2}' | tr -d ' '
+curl -sS -f -I -H "Authorization: token ${COPILOT_ACCESS_LIST}" https://api.github.com | awk -F': ' '/x-oauth-scopes/{print $2}' | tr -d ' '
